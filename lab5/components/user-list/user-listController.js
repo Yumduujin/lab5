d41aacb74b9dda29796ef86444806aba51cd4416
Yumduujin <71216526+Yumduujin@users.noreply.github.com>
2020-12-25 16:04:48 +0800

    Add files via upload

---
+++ lab5/components/user-list/user-listController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+cs142App.controller('UserListController', ['$scope',
+    function ($scope) {
+        $scope.main.title = 'Users';
+
+        console.log('window.cs142models.userListModel()', window.cs142models.userListModel());
+
+        $scope.users = window.cs142models.userListModel();
+    }]);
+
